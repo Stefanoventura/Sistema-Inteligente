64c34c206f90e523d9505d2e1bd6194cf1550b3b
stefano.ventura <stefano.ventura@HCUFU-WK-840229>
2022-09-02 08:10:27 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaIteligente/Form1.Designer.cs
@@ -0,0 +1,377 @@
+﻿namespace SistemaIteligente
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBoxTreinamento = new System.Windows.Forms.GroupBox();
+            this.labelW5 = new System.Windows.Forms.Label();
+            this.labelW4 = new System.Windows.Forms.Label();
+            this.labelW3 = new System.Windows.Forms.Label();
+            this.labelW2 = new System.Windows.Forms.Label();
+            this.labelW1 = new System.Windows.Forms.Label();
+            this.labelCiclos = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonTreinar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBoxTeste_da_rede = new System.Windows.Forms.GroupBox();
+            this.labelDiagnostico = new System.Windows.Forms.Label();
+            this.groupBoxManchas = new System.Windows.Forms.GroupBox();
+            this.radioButtonGrandes = new System.Windows.Forms.RadioButton();
+            this.radioButtonPequenas = new System.Windows.Forms.RadioButton();
+            this.label7 = new System.Windows.Forms.Label();
+            this.buttonTestar = new System.Windows.Forms.Button();
+            this.checkBoxDores = new System.Windows.Forms.CheckBox();
+            this.checkBoxEnjoo = new System.Windows.Forms.CheckBox();
+            this.checkBoxFebre = new System.Windows.Forms.CheckBox();
+            this.groupBoxTreinamento.SuspendLayout();
+            this.groupBoxTeste_da_rede.SuspendLayout();
+            this.groupBoxManchas.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxTreinamento
+            // 
+            this.groupBoxTreinamento.BackColor = System.Drawing.Color.Transparent;
+            this.groupBoxTreinamento.Controls.Add(this.labelW5);
+            this.groupBoxTreinamento.Controls.Add(this.labelW4);
+            this.groupBoxTreinamento.Controls.Add(this.labelW3);
+            this.groupBoxTreinamento.Controls.Add(this.labelW2);
+            this.groupBoxTreinamento.Controls.Add(this.labelW1);
+            this.groupBoxTreinamento.Controls.Add(this.labelCiclos);
+            this.groupBoxTreinamento.Controls.Add(this.label6);
+            this.groupBoxTreinamento.Controls.Add(this.label5);
+            this.groupBoxTreinamento.Controls.Add(this.label4);
+            this.groupBoxTreinamento.Controls.Add(this.label3);
+            this.groupBoxTreinamento.Controls.Add(this.label2);
+            this.groupBoxTreinamento.Controls.Add(this.buttonTreinar);
+            this.groupBoxTreinamento.Controls.Add(this.label1);
+            this.groupBoxTreinamento.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxTreinamento.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.groupBoxTreinamento.Location = new System.Drawing.Point(12, 13);
+            this.groupBoxTreinamento.Name = "groupBoxTreinamento";
+            this.groupBoxTreinamento.Size = new System.Drawing.Size(489, 207);
+            this.groupBoxTreinamento.TabIndex = 1;
+            this.groupBoxTreinamento.TabStop = false;
+            this.groupBoxTreinamento.Text = "Treinamento";
+            this.groupBoxTreinamento.Enter += new System.EventHandler(this.GroupBoxTreinamento_Enter);
+            // 
+            // labelW5
+            // 
+            this.labelW5.AutoSize = true;
+            this.labelW5.Location = new System.Drawing.Point(384, 167);
+            this.labelW5.Name = "labelW5";
+            this.labelW5.Size = new System.Drawing.Size(11, 13);
+            this.labelW5.TabIndex = 12;
+            this.labelW5.Text = "-";
+            // 
+            // labelW4
+            // 
+            this.labelW4.AutoSize = true;
+            this.labelW4.Location = new System.Drawing.Point(371, 134);
+            this.labelW4.Name = "labelW4";
+            this.labelW4.Size = new System.Drawing.Size(11, 13);
+            this.labelW4.TabIndex = 11;
+            this.labelW4.Text = "-";
+            // 
+            // labelW3
+            // 
+            this.labelW3.AutoSize = true;
+            this.labelW3.Location = new System.Drawing.Point(371, 98);
+            this.labelW3.Name = "labelW3";
+            this.labelW3.Size = new System.Drawing.Size(11, 13);
+            this.labelW3.TabIndex = 10;
+            this.labelW3.Text = "-";
+            // 
+            // labelW2
+            // 
+            this.labelW2.AutoSize = true;
+            this.labelW2.Location = new System.Drawing.Point(371, 65);
+            this.labelW2.Name = "labelW2";
+            this.labelW2.Size = new System.Drawing.Size(11, 13);
+            this.labelW2.TabIndex = 9;
+            this.labelW2.Text = "-";
+            // 
+            // labelW1
+            // 
+            this.labelW1.AutoSize = true;
+            this.labelW1.Location = new System.Drawing.Point(371, 28);
+            this.labelW1.Name = "labelW1";
+            this.labelW1.Size = new System.Drawing.Size(11, 13);
+            this.labelW1.TabIndex = 8;
+            this.labelW1.Text = "-";
+            // 
+            // labelCiclos
+            // 
+            this.labelCiclos.AutoSize = true;
+            this.labelCiclos.Location = new System.Drawing.Point(147, 38);
+            this.labelCiclos.Name = "labelCiclos";
+            this.labelCiclos.Size = new System.Drawing.Size(11, 13);
+            this.labelCiclos.TabIndex = 7;
+            this.labelCiclos.Text = "-";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(341, 167);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Wbias";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(341, 134);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "W4";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(341, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "W3";
+            this.label4.Click += new System.EventHandler(this.Label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(341, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "W2";
+            this.label3.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(341, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "W1";
+            // 
+            // buttonTreinar
+            // 
+            this.buttonTreinar.BackColor = System.Drawing.SystemColors.Window;
+            this.buttonTreinar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTreinar.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.buttonTreinar.Location = new System.Drawing.Point(21, 162);
+            this.buttonTreinar.Name = "buttonTreinar";
+            this.buttonTreinar.Size = new System.Drawing.Size(75, 25);
+            this.buttonTreinar.TabIndex = 1;
+            this.buttonTreinar.Text = "Treinar";
+            this.buttonTreinar.UseVisualStyleBackColor = false;
+            this.buttonTreinar.Click += new System.EventHandler(this.ButtonTreinar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quantidade de Ciclos:";
+            // 
+            // groupBoxTeste_da_rede
+            // 
+            this.groupBoxTeste_da_rede.BackColor = System.Drawing.Color.Transparent;
+            this.groupBoxTeste_da_rede.Controls.Add(this.labelDiagnostico);
+            this.groupBoxTeste_da_rede.Controls.Add(this.groupBoxManchas);
+            this.groupBoxTeste_da_rede.Controls.Add(this.label7);
+            this.groupBoxTeste_da_rede.Controls.Add(this.buttonTestar);
+            this.groupBoxTeste_da_rede.Controls.Add(this.checkBoxDores);
+            this.groupBoxTeste_da_rede.Controls.Add(this.checkBoxEnjoo);
+            this.groupBoxTeste_da_rede.Controls.Add(this.checkBoxFebre);
+            this.groupBoxTeste_da_rede.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxTeste_da_rede.Location = new System.Drawing.Point(13, 250);
+            this.groupBoxTeste_da_rede.Name = "groupBoxTeste_da_rede";
+            this.groupBoxTeste_da_rede.Size = new System.Drawing.Size(489, 222);
+            this.groupBoxTeste_da_rede.TabIndex = 2;
+            this.groupBoxTeste_da_rede.TabStop = false;
+            this.groupBoxTeste_da_rede.Text = "Teste da Rede";
+            // 
+            // labelDiagnostico
+            // 
+            this.labelDiagnostico.AutoSize = true;
+            this.labelDiagnostico.Location = new System.Drawing.Point(380, 170);
+            this.labelDiagnostico.Name = "labelDiagnostico";
+            this.labelDiagnostico.Size = new System.Drawing.Size(23, 13);
+            this.labelDiagnostico.TabIndex = 13;
+            this.labelDiagnostico.Text = "----";
+            this.labelDiagnostico.Click += new System.EventHandler(this.Label9_Click);
+            // 
+            // groupBoxManchas
+            // 
+            this.groupBoxManchas.Controls.Add(this.radioButtonGrandes);
+            this.groupBoxManchas.Controls.Add(this.radioButtonPequenas);
+            this.groupBoxManchas.Location = new System.Drawing.Point(239, 44);
+            this.groupBoxManchas.Name = "groupBoxManchas";
+            this.groupBoxManchas.Size = new System.Drawing.Size(210, 70);
+            this.groupBoxManchas.TabIndex = 5;
+            this.groupBoxManchas.TabStop = false;
+            this.groupBoxManchas.Text = "Manchas";
+            // 
+            // radioButtonGrandes
+            // 
+            this.radioButtonGrandes.AutoSize = true;
+            this.radioButtonGrandes.Location = new System.Drawing.Point(104, 42);
+            this.radioButtonGrandes.Name = "radioButtonGrandes";
+            this.radioButtonGrandes.Size = new System.Drawing.Size(72, 17);
+            this.radioButtonGrandes.TabIndex = 1;
+            this.radioButtonGrandes.TabStop = true;
+            this.radioButtonGrandes.Text = "Grandes";
+            this.radioButtonGrandes.UseVisualStyleBackColor = true;
+            this.radioButtonGrandes.CheckedChanged += new System.EventHandler(this.RadioButton2_CheckedChanged);
+            // 
+            // radioButtonPequenas
+            // 
+            this.radioButtonPequenas.AutoSize = true;
+            this.radioButtonPequenas.Location = new System.Drawing.Point(6, 42);
+            this.radioButtonPequenas.Name = "radioButtonPequenas";
+            this.radioButtonPequenas.Size = new System.Drawing.Size(81, 17);
+            this.radioButtonPequenas.TabIndex = 0;
+            this.radioButtonPequenas.TabStop = true;
+            this.radioButtonPequenas.Text = "Pequenas";
+            this.radioButtonPequenas.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(305, 170);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(74, 13);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Diagnóstico";
+            // 
+            // buttonTestar
+            // 
+            this.buttonTestar.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonTestar.Enabled = false;
+            this.buttonTestar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTestar.ForeColor = System.Drawing.Color.Red;
+            this.buttonTestar.Location = new System.Drawing.Point(20, 191);
+            this.buttonTestar.Name = "buttonTestar";
+            this.buttonTestar.Size = new System.Drawing.Size(75, 25);
+            this.buttonTestar.TabIndex = 3;
+            this.buttonTestar.Text = "Testar";
+            this.buttonTestar.UseVisualStyleBackColor = false;
+            this.buttonTestar.Click += new System.EventHandler(this.ButtonTestar_Click);
+            // 
+            // checkBoxDores
+            // 
+            this.checkBoxDores.AutoSize = true;
+            this.checkBoxDores.Location = new System.Drawing.Point(20, 125);
+            this.checkBoxDores.Name = "checkBoxDores";
+            this.checkBoxDores.Size = new System.Drawing.Size(59, 17);
+            this.checkBoxDores.TabIndex = 2;
+            this.checkBoxDores.Text = "Dores";
+            this.checkBoxDores.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxEnjoo
+            // 
+            this.checkBoxEnjoo.AutoSize = true;
+            this.checkBoxEnjoo.Location = new System.Drawing.Point(20, 87);
+            this.checkBoxEnjoo.Name = "checkBoxEnjoo";
+            this.checkBoxEnjoo.Size = new System.Drawing.Size(58, 17);
+            this.checkBoxEnjoo.TabIndex = 1;
+            this.checkBoxEnjoo.Text = "Enjôo";
+            this.checkBoxEnjoo.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxFebre
+            // 
+            this.checkBoxFebre.AutoSize = true;
+            this.checkBoxFebre.Location = new System.Drawing.Point(20, 44);
+            this.checkBoxFebre.Name = "checkBoxFebre";
+            this.checkBoxFebre.Size = new System.Drawing.Size(58, 17);
+            this.checkBoxFebre.TabIndex = 0;
+            this.checkBoxFebre.Text = "Febre";
+            this.checkBoxFebre.UseVisualStyleBackColor = true;
+            this.checkBoxFebre.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::SistemaIteligente.Properties.Resources.hospital;
+            this.ClientSize = new System.Drawing.Size(523, 485);
+            this.Controls.Add(this.groupBoxTeste_da_rede);
+            this.Controls.Add(this.groupBoxTreinamento);
+            this.Font = new System.Drawing.Font("Microsoft PhagsPa", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Sistema Inteligente";
+            this.groupBoxTreinamento.ResumeLayout(false);
+            this.groupBoxTreinamento.PerformLayout();
+            this.groupBoxTeste_da_rede.ResumeLayout(false);
+            this.groupBoxTeste_da_rede.PerformLayout();
+            this.groupBoxManchas.ResumeLayout(false);
+            this.groupBoxManchas.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxTreinamento;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonTreinar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBoxTeste_da_rede;
+        private System.Windows.Forms.CheckBox checkBoxFebre;
+        private System.Windows.Forms.GroupBox groupBoxManchas;
+        private System.Windows.Forms.RadioButton radioButtonGrandes;
+        private System.Windows.Forms.RadioButton radioButtonPequenas;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button buttonTestar;
+        private System.Windows.Forms.CheckBox checkBoxDores;
+        private System.Windows.Forms.CheckBox checkBoxEnjoo;
+        private System.Windows.Forms.Label labelW5;
+        private System.Windows.Forms.Label labelW4;
+        private System.Windows.Forms.Label labelW3;
+        private System.Windows.Forms.Label labelW2;
+        private System.Windows.Forms.Label labelW1;
+        private System.Windows.Forms.Label labelCiclos;
+        private System.Windows.Forms.Label labelDiagnostico;
+    }
+}
+
---
+++ SistemaIteligente/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaIteligente
+{
+    public partial class Form1 : Form
+    {
+        double[,] x_VetoresTreinamento = new double[,] { { 1, 1, 1, 1, 1},
+                                                 {-1, -1, -1, -1, 1},
+                                                 {1, 1, 1, -1, 1},
+                                                 {1, -1, -1, 1, 1},
+                                                 {1, -1, 1, 1, 1},
+                                                 {-1, -1, -1, 1, 1} };
+
+        double[] Y_SaidaDesejada = new double[] { 1, -1, -1, 1, -1, 1 };
+        double[] W_Pesos = new double[] { 0, 0, 0, 0, 0 };
+
+        double Erro;
+        double TaxaAprendizagem = 0.2;
+        int Ciclos;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonTreinar_Click(object sender, EventArgs e)
+        {
+
+            buttonTestar.Enabled = true;
+
+            Ciclos = 0;
+            bool TemErro = true;
+
+            while (TemErro == true)
+            {
+                TemErro = false;
+
+                for (int i = 0; i < 6; i++)
+                {
+                    double somatorio = 0;
+
+                    double RespostaDaRede;
+                    for (int j = 0; j < 5; j++)
+                        somatorio += x_VetoresTreinamento[i, j] * W_Pesos[j];
+
+                    if (somatorio >= 0)
+                        RespostaDaRede = 1;
+                    else RespostaDaRede = -1;
+
+                    Erro = Y_SaidaDesejada[i] - RespostaDaRede;
+                    if (Erro != 0)
+                        TemErro = true;
+
+                    for (int J = 0; J < 5; J++)
+                    {
+                        Double DeltaW = x_VetoresTreinamento[i, J] * Erro * TaxaAprendizagem;
+                        W_Pesos[J] = W_Pesos[J] + DeltaW;
+                    }
+                }
+
+                Ciclos++;
+            }
+            labelCiclos.Text = Convert.ToString(Ciclos);
+            labelW1.Text = Convert.ToString(W_Pesos[0]);
+            labelW2.Text = Convert.ToString(W_Pesos[1]);
+            labelW3.Text = Convert.ToString(W_Pesos[2]);
+            labelW4.Text = Convert.ToString(W_Pesos[3]);
+            labelW5.Text = Convert.ToString(W_Pesos[4]);
+        }
+
+        private void GroupBoxTreinamento_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonTestar_Click(object sender, EventArgs e)
+        {
+            double febre, enjoo, dores, manchas;
+
+            if (checkBoxFebre.Checked)
+                febre = 1;
+            else febre = -1;
+
+            if (checkBoxDores.Checked)
+                dores = 1;
+            else dores = -1;
+
+            if (checkBoxEnjoo.Checked)
+                enjoo = 1;
+            else enjoo = -1;
+
+            if (radioButtonPequenas.Checked)
+                manchas = 1;
+            else manchas = 1;
+
+            double somatorio;
+            somatorio = (W_Pesos[0] * febre) + (W_Pesos[1] * enjoo) + (W_Pesos[2] * manchas) + (W_Pesos[3] * dores) + (1 * W_Pesos[4]);
+
+            if (somatorio >= 0)
+                labelDiagnostico.Text = "Doente";
+            else labelDiagnostico.Text = "Saudável";
+        }
+    }
+}
